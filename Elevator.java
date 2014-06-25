447b12c22f82fa27bf282b759971e997f503acde
Jereneal <jereneal20@gmail.com>
2014-06-25 15:35:14 +0900

    Enter fixed

--- Elevator.java
+++ Elevator.java
@@ -5,9 +5,9 @@ public class Elevator {
 	private int currentFloor;
 	private int maxLoad;
 	private boolean gateOpen;
-	
+
 	private ArrayList<Person> passengers;
-	
+
 	public Elevator(int maxFloor, int maxLoad) {
 		// 해당 class 초기화
 		this.passengers = new ArrayList<Person>();
@@ -16,7 +16,7 @@ public class Elevator {
 		this.gateOpen = false;
 		this.currentFloor = 1;
 	}
-	
+
 	public void move(int floor) {
 		// 1층 ~ maxFloor로 이동
 		if (floor <= maxFloor && floor > 0) {
@@ -24,34 +24,34 @@ public class Elevator {
 		} else
 			System.out.println("그런 층 없습니다.");
 	}
-	
+
 	public void enter(Person person) {
 		// 전체 탑승객 + 새로운 탑승객의 무게가 maxLoad를 넘지 않고, 열려있으면 탑승
 		int curWeight = 0;
-		
-		for(int i = 0 ; i< passengers.size() ; i++){
+
+		for (int i = 0; i < passengers.size(); i++) {
 			curWeight += passengers.get(i).getWeight();
 			curWeight += person.getWeight();
 		}
-		if(curWeight > maxLoad){
+		if (curWeight > maxLoad) {
 			System.out.println("cannot enter the Elevator - Full weight");
 			curWeight -= person.getWeight();
 			System.exit(0);
 		}
-		if(gateOpen == false){
+		if (gateOpen == false) {
 			System.out.println("cannot enter the Elevator - door close");
 			System.exit(0);
 		}
-		
-		System.out.println("Enter success");
+
+		System.out.println("Enter success " + person.getName());
 		passengers.add(person);
-		
+
 	}
-	
+
 	public void leave(Person person) {
 		// 열려있고, person이 있으면 내림
 	}
-	
+
 	public void setOpen(boolean Open) {
 	}
 
@@ -65,11 +65,11 @@ public class Elevator {
 					.println("Please Press the open butten. Now this gate is closed");
 		}
 	}
-	
+
 	public void emergency(String message) {
 		// ????
 	}
-	
+
 	public void printInfo() {
 		// 엘리베이터의 상태(층, 탑승객, 등) 출력
 		System.out.println("Current Floor : " + currentFloor);
